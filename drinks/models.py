0a8769321cbc5384470eb8d620de86acb2b8a53d
George Barrett <georgeaustinbarrett@gmail.com>
2022-10-26 12:24:50 +0100

    post requests set up

--- drinks/models.py
+++ drinks/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# this is how django know that 'drinks' is a model class
+# this is how django knows that 'drinks' is a model class
 class Drink(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
